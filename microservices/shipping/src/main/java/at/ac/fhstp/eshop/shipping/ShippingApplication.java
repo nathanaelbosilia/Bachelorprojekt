25b162e0e787adaa2d3a50197cabff7230c6c3ea
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-19 20:00:29 +0100

    feat: enable configuration loading

--- microservices/shipping/src/main/java/at/ac/fhstp/eshop/shipping/ShippingApplication.java
+++ microservices/shipping/src/main/java/at/ac/fhstp/eshop/shipping/ShippingApplication.java
@@ -1,9 +1,12 @@
 package at.ac.fhstp.eshop.shipping;
 
+import at.ac.fhstp.eshop.shipping.configuration.KafkaTopicNames; // TODO: fix package visibilities -> .shipping.
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({KafkaTopicNames.class})
 public class ShippingApplication {
 
 	public static void main(String[] args) {
