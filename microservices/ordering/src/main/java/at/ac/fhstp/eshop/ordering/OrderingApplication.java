4cce5ae9ecd4104ba70d7caf25617af80e556af9
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-19 19:58:59 +0100

    feat: add annotation for enabling configuration loading

--- microservices/ordering/src/main/java/at/ac/fhstp/eshop/ordering/OrderingApplication.java
+++ microservices/ordering/src/main/java/at/ac/fhstp/eshop/ordering/OrderingApplication.java
@@ -1,13 +1,16 @@
 package at.ac.fhstp.eshop.ordering;
 
+import at.ac.fhstp.eshop.ordering.configuration.KafkaTopicNames;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({KafkaTopicNames.class})
 public class OrderingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderingApplication.class, args);
+    }
 
 }
