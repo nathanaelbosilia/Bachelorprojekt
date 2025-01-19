966cb50d4282ac6006ae42338cc24a714ad9ae45
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-19 20:05:40 +0100

    feat: add kafka listener for created orders

---
+++ microservices/shipping/src/main/java/at/ac/fhstp/eshop/shipping/events/OrderEventConsumer.java
@@ -0,0 +1,21 @@
+package at.ac.fhstp.eshop.shipping.events;
+
+import at.ac.fhstp.eshop.shipping.shipment.ShipmentService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderEventConsumer {
+
+    private final ShipmentService shipmentService;
+
+    public OrderEventConsumer(ShipmentService shipmentService) {
+        this.shipmentService = shipmentService;
+    }
+
+    @KafkaListener(topics = "${spring.kafka.topics.order-created}", containerFactory = "orderCreatedEventListener")
+    public void consumeOrderCreatedEvent(OrderCreatedEvent orderCreatedEvent) {
+        System.out.println("Received OrderCreatedEvent: " + orderCreatedEvent.id());
+        shipmentService.createShipment(orderCreatedEvent.orderDto());
+    }
+}
