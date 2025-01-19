953972f8e27f372fb37d0a437f1df26f03f3a3d0
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-19 20:02:50 +0100

    build: add jpa repositories

---
+++ microservices/shipping/src/main/java/at/ac/fhstp/eshop/shipping/courier/CourierRepository.java
@@ -0,0 +1,13 @@
+package at.ac.fhstp.eshop.shipping.courier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CourierRepository extends JpaRepository<Courier, UUID> {
+
+    Optional<Courier> findByName(String name);
+}
