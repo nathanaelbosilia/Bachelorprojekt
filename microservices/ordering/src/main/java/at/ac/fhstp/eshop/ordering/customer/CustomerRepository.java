48b8f6c763b7b9b14600d5293b086c77d0d7b3fb
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-19 19:57:42 +0100

    feat: add jpa repositories

---
+++ microservices/ordering/src/main/java/at/ac/fhstp/eshop/ordering/customer/CustomerRepository.java
@@ -0,0 +1,10 @@
+package at.ac.fhstp.eshop.ordering.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
