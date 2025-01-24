e990dfb944783651e72e491b390c1dc33a7c40e6
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-25 00:12:25 +0100

    feat: call notification service in kafka consumers

--- microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/events/OrderEventConsumer.java
+++ microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/events/OrderEventConsumer.java
@@ -1,13 +1,21 @@
 package at.ac.fhstp.eshop.notification.events;
 
+import at.ac.fhstp.eshop.notification.NotificationService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderEventConsumer {
 
+    private final NotificationService notificationService;
+
+    public OrderEventConsumer(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @KafkaListener(topics = "${spring.kafka.topics.order-created}", containerFactory = "orderCreatedEventListener")
     public void consumeOrderCreatedEvent(OrderCreatedEvent orderCreatedEvent) {
         System.out.println("Received OrderCreatedEvent: " + orderCreatedEvent.id());
+        notificationService.sendOrderConfirmationNotification(orderCreatedEvent);
     }
 }
--- microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/events/ShipmentEventConsumer.java
+++ microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/events/ShipmentEventConsumer.java
@@ -1,13 +1,21 @@
 package at.ac.fhstp.eshop.notification.events;
 
+import at.ac.fhstp.eshop.notification.NotificationService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShipmentEventConsumer {
 
+    private final NotificationService notificationService;
+
+    public ShipmentEventConsumer(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @KafkaListener(topics = "${spring.kafka.topics.shipment-created}", containerFactory = "shipmentCreatedEventListener")
     public void consumeShipmentCreatedEvent(ShipmentCreatedEvent shipmentCreatedEvent) {
         System.out.println("Received ShipmentCreatedEvent: " + shipmentCreatedEvent.id());
+        notificationService.sendShipmentNotification(shipmentCreatedEvent);
     }
 }
