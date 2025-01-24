7963fbb997b1e62506cce4e5ce272eab6e894ca6
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-01-24 23:53:15 +0100

    feat: enable configuration loading

--- microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/NotificationApplication.java
+++ microservices/notification/src/main/java/at/ac/fhstp/eshop/notification/NotificationApplication.java
@@ -1,13 +1,16 @@
 package at.ac.fhstp.eshop.notification;
 
+import at.ac.fhstp.eshop.notification.configuration.KafkaTopicNames;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({KafkaTopicNames.class})
 public class NotificationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
 
 }
