ab6d01c67be27d690727b1ddf61c6b834b822067
Nathanael Bosilia <nathanael.bosilia@hotmail.de>
2025-02-14 22:48:56 +0100

    feat: add new k6 test for constant arrival rate runner

---
+++ k6/create-orders-test-constant-arrival-rate.ts
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import {Trend} from "k6/metrics";
+
+const http_reqs_201 = new Trend('http_reqs_201');
+
+export const options = {
+    // https://grafana.com/docs/k6/latest/using-k6/scenarios/#scenario-executors
+    scenarios: {
+        createOrder_shared_iterations: {
+            executor: 'constant-arrival-rate',
+            rate: 10,
+            timeUnit: '1s',
+            duration: '2m',
+            preAllocatedVUs: 20
+        }
+    }
+}
+export default function () {
+    const PORT = __ENV.PORT || "8080";
+    const url = `http://localhost:${PORT}/api/orders`;
+
+    const headers = {
+        'Content-Type': 'application/json'
+    };
+
+    const payload = JSON.stringify({
+        "articleId": "25984125-ebb5-4d43-81a7-033bee9cf6df",
+        "customerId": "b8b4a824-89d0-432a-ae60-718928d78e99",
+        "quantity": 0
+    });
+
+    const response = http.post(url, payload, {headers: headers});
+    if (response.status == 201) {
+        http_reqs_201.add(response.timings.duration);
+    }
+}
